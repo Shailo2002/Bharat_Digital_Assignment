f49cd1bfc97de939d74e13af752451bdca8c5635
shailesh <shailesh364465@gmail.com>
2025-10-29 22:15:17 +0530

    forntend url option added in cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: ["http://localhost:5173", process.env.FRONTEND_URL],
     credentials: true,
   })
 );
